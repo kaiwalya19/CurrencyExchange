cbe352b028c7baf8b8f77ebf4196c404b81965b9
kaiwalya <kaiwalyakshirsagar@gmail.com>
2024-11-26 14:07:18 +0530

    Update urls.py

--- mycurrency/urls.py
+++ mycurrency/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from mycurrency import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/currency/', views.CurrencyListCreateView.as_view(), name='currency-list-create'),
     path('api/currency/<int:pk>/', views.CurrencyRetrieveUpdateDestroyView.as_view(), name='currency-detail'),
     path('api/exchange-rate/', views.CurrencyRateListView.as_view(), name='exchange-rate-list'),
